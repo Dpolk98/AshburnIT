a359384cf5aeceb0b0ae66fa90f3a0cc243e3ca8
Daniel Polk <Danielpolk7@icloud.com>
2023-12-17 18:57:21 -0500

    initial commit

---
+++ Ashburn IT Solutions/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar () {
+    return (
+        <>
+            <div>
+                <h1>Ashburn IT Solutions</h1>
+                <img src='../assets/logo'>
+                </img>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
