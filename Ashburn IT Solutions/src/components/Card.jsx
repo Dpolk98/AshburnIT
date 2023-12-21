8ea005b13ead5dded32de7a431e6b36be4316c70
Daniel Polk <Danielpolk7@icloud.com>
2023-12-21 13:49:07 -0500

    created function to iteratively create cards

--- Ashburn IT Solutions/src/components/Card.jsx
+++ Ashburn IT Solutions/src/components/Card.jsx
@@ -1,17 +1,16 @@
-
-
 export function Card() {
     // const topic = ['Data', 'Commercial', 'Web', 'Contact'];
-    const topic = 'Data';
+    const topic = ['Data', 'Industry', 'Commercial', 'Get Fucked'];
 
     return (
-        <div class="card">
-            <div>
-                <h3>{"Example: " + topic}</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum condimentum lacinia. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris libero dui, finibus a nibh in, finibus posuere augue. Ut sed ullamcorper nulla. Vivamus in ultrices lectus. Donec eu euismod massa, quis tincidunt dui. Morbi tempus semper massa. Quisque quis tempus nisl, et placerat leo. Sed at dapibus quam.
 
-Suspendisse in sem suscipit, vestibulum nisl at, ornare nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla maximus ligula in ipsum porttitor dignissim. Duis eu dui scelerisque, ultrices ligula id, lobortis mauris. Nam lorem nunc, molestie in porta vestibulum, vestibulum id mi. Aliquam non leo risus. Maecenas arcu elit, hendrerit ut quam vel, posuere ornare purus.</p>
+        <div class="container">
+            {topic.map((cardTitle, topicDesc) => (
+            <div class='card'>
+                <h3>{cardTitle}</h3>
+                <p>{topicDesc}</p>
             </div>
+            ))}
         </div>
     )
 };
